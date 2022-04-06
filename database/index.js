bd97f2dd3ec6020539cdab817bccbb2306321e92
Rafael Massao Myauti <myauti.rafa@outlook.com>
2022-04-06 19:22:00 -0400

    Created database connection management

--- database/index.js
+++ database/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect('your_connection_string_here').then(()=>{
-    console.log('Database: Connected successfully!');
+    console.log('Database: Connected successfully');
 }).catch(error => console.log(error));
 mongoose.Promise = global.Promise;
 
