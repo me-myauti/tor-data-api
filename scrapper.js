5ac459c0d5f57acd5956a87fcac801ac7bd9713b
Rafael Massao Myauti <myauti.rafael@outlook.com>
2022-04-09 20:38:45 -0400

    Fixed flow error

--- scrapper.js
+++ scrapper.js
@@ -1,14 +1,17 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
+var path = "./ips.json";
+
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  });
   const page = await browser.newPage();
   const response = await page.goto('https://www.dan.me.uk/torlist/');
   if(response.status() == 200){
       const ipList = await page.evaluate(()=>{
       const nodeList = document.querySelectorAll('pre')[0].innerText
       const ipArray = [nodeList]
-  
       const list = ipArray.map(ips => ({
         ip: ips
       }))
@@ -19,11 +22,29 @@ const fs = require('fs');
       return separatedList
     })
   
-    fs.writeFile('ips.json', JSON.stringify(ipList, null, 2), err => {
-       if(err)throw new Error('Could not create json file!')
-      console.log('Data inserted successfully')
-    })
+    try{
+        fs.accessSync(path, fs.constants.F_OK)
+        fs.writeFile('ips.json', JSON.stringify(ipList, null, 2), err => {
+            if(err)throw new Error('Could not create json file!')
+           console.log('Created ips.json')
+         })
+
+    }catch(error){
+        fs.writeFile('ips.json', JSON.stringify(ipList, null, 2), err => {
+            if(err)throw new Error('Could not create json file!')
+           console.log('Created ips.json')
+         })
+    }
   }else{
+    const defaultData = [{ip: "Theres no ips yet"}]
+    try {
+        fs.accessSync(path, fs.constants.F_OK);
+    }catch (error) {
+        fs.writeFile('ips.json', JSON.stringify(defaultData, null, 2), err => {
+            if(err)throw new Error('Could not create json file!')
+           console.log('Created ips.json')
+         })
+    }
     console.log('Error rewriting list of ips: You need to wait 30 minutes until the next request\n You should check your ips.json file instead')
   }
   await browser.close();
